aaea51eeb352f420c49aff29ff3961dfdd87ab11
Marco Ravaioli <MARCO.RAVAIOLI@local.ittcesena.it>
2026-03-13 09:31:39 +0100

    R0.2 Codice completato

--- GeneraParole/GeneraParole/MainWindow.xaml.cs
+++ GeneraParole/GeneraParole/MainWindow.xaml.cs
@@ -1,34 +1,66 @@
-﻿using System.Text;
+﻿using System;
+using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GeneraParole
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-
-        private string lettere = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        private string parolaCorrente = "";
+        private string parolaInCorso = "";
+        private const string Alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         private Random rnd = new Random();
+        private bool staRuotando = true;
 
         public MainWindow()
         {
             InitializeComponent();
+            AvviaRotazioneLettere();
+        }
+
+        //Fa ruotare le lettere senza bloccare il programma
+        private async void AvviaRotazioneLettere()
+        {
+            while (staRuotando)
+            {
+                // Sceglie una lettera a caso
+                char letteraCasuale = Alfabeto[rnd.Next(Alfabeto.Length)];
+
+                // La mostra a video
+                LetteraCorrente.Text = letteraCasuale.ToString();
+
+                // Aspetta 100 millisecondi 
+                await Task.Delay(100);
+            }
         }
 
         private void BtnSorteggia_Click(object sender, RoutedEventArgs e)
         {
+            //Prima controlliamo se è vuoto (molto importante per il tuo requisito)
+            if (TxtLength.Text == "")
+            {
+                MessageBox.Show("Il campo lunghezza è vuoto! Inserisci un numero.");
+                return;
+            }
 
+            // Proviamo a convertire in numero e verifichiamo che sia > 0
+            if (int.TryParse(TxtLength.Text, out int lunghezzaScelta) && lunghezzaScelta > 0)
+            {
+                string letteraSorteggiata = LetteraCorrente.Text;
+                parolaInCorso += letteraSorteggiata;
+                ParolaCorrente.Text = "Parola in corso: " + parolaInCorso;
+
+                if (parolaInCorso.Length >= lunghezzaScelta)
+                {
+                    LstParole.Items.Add(parolaInCorso);
+                    parolaInCorso = "";
+                    ParolaCorrente.Text = "Parola in corso: ";
+                    MessageBox.Show("Parola completata e salvata!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Inserisci una lunghezza valida (maggiore di 0)!");
+            }
         }
     }
 }
